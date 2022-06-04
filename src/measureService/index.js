21a078be5b5707c0eadc3afd09bfce64117de2a4
Tiffany <tiffany831101@gmail.com>
2022-06-04 15:06:27 +0800

    Make the params of the payload more flexible

--- src/measureService/index.js
+++ src/measureService/index.js
@@ -5,17 +5,55 @@ const uuidv4 = require('uuid').v4;
 
 const types = ['SHARON', 'RIB_EYE'];
 
+const paramsForTypes = (id) => ([
+  {
+    id,
+    type: 'SHARON',
+    thickness: 2 + Math.random().toFixed(2),
+    moisture: 6 + Math.random().toFixed(2),
+  },
+  {
+    id,
+    type: 'RIB_EYE',
+    thickness: 2 + Math.random().toFixed(2),
+    moisture: 6 + Math.random().toFixed(2),
+  },
+])
+
+class PayloadManager { 
+  constructor() { 
+    this.steakType = null;
+  }
+
+  set setType(params) {
+    this.steakType = params;
+  }
+
+  getData() { 
+    return this.steakType._payload;
+  };
+}
+
+
+class PayloadType { 
+  constructor(params) { 
+    this._payload = params;
+  }
+}
+
+const payloadManager = new PayloadManager();
+
 const run = async () => {
   const handler = setInterval(async () => {
     const index = Math.floor((Math.random() * 10) % types.length);
     const id = uuidv4();
+    
+    // if need to add new payload type => add into paramsForTypes
+    const paramsForPayloadType = paramsForTypes(id)[index];
+    const payloadType = new PayloadType(paramsForPayloadType);
 
-    const payload = {
-      id,
-      type: types[index],
-      thickness: 2 + Math.random().toFixed(2),
-      moisture: 6 + Math.random().toFixed(2),
-    };
+    payloadManager.setType = payloadType;
+    const payload = payloadManager.getData();
 
     const { data } = await axios.post(`${domainService.apc.endpoint}/api/v1/process`, payload);
   }, cron.measurePeriod);
