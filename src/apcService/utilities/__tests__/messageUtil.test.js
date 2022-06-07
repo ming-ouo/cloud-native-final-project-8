a242f2e3137ad04b5a9a59e865429f76961733f3
Ming <ming@alone.tw>
2022-06-08 00:42:16 +0800

    complete messageUtil unit test

--- src/apcService/utilities/__tests__/messageUtil.test.js
+++ src/apcService/utilities/__tests__/messageUtil.test.js
@@ -2,10 +2,24 @@ const { natsMessageHandler } = require('../messageUtil');
 
 describe('Module messageUtil', () => {
   const fakeType = 'FACTOR_THICKNESS';
+  const fakeType2 = 'FACTOR_MOISTURE';
   const fakeFactor = 0.5;
 
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+
+  it('Method natsMessageHandler for global.cache = undefined', async () => {
+    global.cache = undefined
+
+    let ret = natsMessageHandler(
+      JSON.stringify({
+        type: fakeType,
+        factor: fakeFactor,
+      })
+    );
+
+    expect(ret).toBe(undefined);
   });
 
   it('Method natsMessageHandler for success', async () => {
@@ -21,6 +35,21 @@ describe('Module messageUtil', () => {
     );
 
     expect(global.cache.set).toHaveBeenCalledWith(fakeType, fakeFactor);
+  });
+
+  it('Method natsMessageHandler for else if branch', async () => {
+    global.cache = {
+      set: jest.fn().mockReturnValueOnce(true),
+    };
+
+    natsMessageHandler(
+      JSON.stringify({
+        type: fakeType2,
+        factor: fakeFactor,
+      })
+    );
+
+    expect(global.cache.set).toHaveBeenCalledWith(fakeType2, fakeFactor);
   });
 
   it('Method natsMessageHandler for failed', async () => {
